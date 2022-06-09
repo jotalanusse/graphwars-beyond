19ea9ccac7657229d90eb5de90bacd53e63371ae
Jota Lanusse <jotalanusse@gmail.com>
2022-06-09 02:36:29 +0200

    Update CameraFollow.cs

--- Assets/Camera/CameraFollow.cs
+++ Assets/Camera/CameraFollow.cs
@@ -1,11 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float speed = 0.1f;
-    public float mapSize = 12;
+    public float speed = 0.5f;
+    // public float mapSize = 12;
 
     void Start()
     {
@@ -20,11 +21,20 @@ public class CameraFollow : MonoBehaviour
     void Follow()
     {
         Vector3 mapCenter = new Vector3(0, 0, -10);
-        float distanceFromCenter = Vector2.Distance(transform.position, mapCenter);
+        Vector2 cameraPosition = transform.position;
 
-        if (distanceFromCenter > 12)
+        Camera camera = transform.GetComponent<Camera>();
+
+        // TODO: Fix this whole shit
+        if (Math.Abs(cameraPosition.y) > (20 - camera.orthographicSize) * 0.8)
         {
-            Vector3 smoothPosition = Vector3.Lerp(transform.position, mapCenter, speed * Time.fixedDeltaTime);
+            Vector3 smoothPosition = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, mapCenter.y, speed * Time.fixedDeltaTime), transform.position.z);
+            transform.position = smoothPosition;
+        }
+
+        if (Math.Abs(cameraPosition.x) > (20 - camera.orthographicSize) * camera.aspect)
+        {
+            Vector3 smoothPosition = new Vector3(Mathf.Lerp(transform.position.x, mapCenter.x, speed * Time.fixedDeltaTime), transform.position.y, transform.position.z);
             transform.position = smoothPosition;
         }
     }
