32f4723ef5c1a5d9b1d00ac56ce01b29107b7abf
Jota Lanusse <jotalanusse@gmail.com>
2022-06-10 12:36:49 +0200

    Minor changes to the projectile

--- Assets/Gun/Projectile/BaseProjectile.cs
+++ Assets/Gun/Projectile/BaseProjectile.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 
 public class BaseProjectile : MonoBehaviour
 {
-    public float speed = 80f;
-    public int waypointsPerFrame = 1;
-    private float waypointDistanceThresholdX = 1f;
-    private float waypointDistanceThresholdY = 10f;
+    public float speed = 20f;
+    private float waypointDistanceThreshold = 0.3f;
 
     public List<Vector2> waypoints = new List<Vector2>();
     private int currentWaypointIndex = 0;
@@ -30,42 +28,24 @@ public class BaseProjectile : MonoBehaviour
 
     void UpdatePosition()
     {
-        // TODO: Transform this to be constant 
-        for (int i = 0; i < waypointsPerFrame; i += 1)
+        Rigidbody2D projectileRigidbody = transform.GetComponent<Rigidbody2D>();
+
+        Vector2 currentWaypoint = waypoints[currentWaypointIndex];
+        Vector2 direction = currentWaypoint - (Vector2)transform.position;
+
+        projectileRigidbody.velocity = direction.normalized * speed;
+
+        // If we are close enough continue to the next one
+        float waypointDistance = Vector2.Distance(transform.position, currentWaypoint);
+        if (waypointDistance <= waypointDistanceThreshold)
         {
-            Vector2 currentWaypoint = waypoints[currentWaypointIndex];
-
-            // float waypointDistanceX = Math.Abs(currentWaypoint.x - transform.position.x);
-            // float waypointDistanceY = Math.Abs(currentWaypoint.y - transform.position.y);
-
-            // If the current waypoint is very far the function is probably not continuous (not viable)
-            /*            if (waypointDistanceX <= waypointDistanceThresholdX && waypointDistanceY <= waypointDistanceThresholdY)
-                        {
-
-                        }
-                        else
-                        {
-                            // Debug.Log("Distance to next waypoint was: " + waypointDistanceX + ":" + waypointDistanceY); // TODO: Remove
-                            Despawn(5); // TODO: Change
-                        }*/
-
-            Rigidbody2D projectileRigidbody = transform.GetComponent<Rigidbody2D>();
-
-            Vector2 direction = currentWaypoint - (Vector2)transform.position;
-            projectileRigidbody.velocity = direction.normalized * speed;
-
-            // If we are close enough vontinue to the next one
-            float waypointDistance = Vector2.Distance(transform.position, currentWaypoint);
-            if (waypointDistance <= 0.3)
+            if (currentWaypointIndex < waypoints.Count - 1)
             {
-                if (currentWaypointIndex < waypoints.Count - 1)
-                {
-                    currentWaypointIndex += 1;
-                }
-                else
-                {
-                    Destroy(gameObject, 5); // TODO: Change
-                }
+                currentWaypointIndex += 1;
+            }
+            else
+            {
+                Destroy(gameObject, 5); // TODO: Change
             }
         }
     }
